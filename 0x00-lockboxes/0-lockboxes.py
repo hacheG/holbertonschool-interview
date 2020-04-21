6b63d10cde8d89ca3bf468e9d86b43e5a359815c
HacheG <943@holbertonschool.com>
2020-04-21 16:59:49 -0500

    lock

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+#comentario
+
+def canUnlockAll(boxes):
+    """comentario de funcion"""
+    key = 0
+    keychain = [0]
+    if len(boxes) is 0:
+        return True
+    for key in keychain:
+        for key in boxes[key]:
+            if key in keychain:
+                continue
+            else:
+                if key <= len(boxes) - 1:
+                    keychain.append(key)
+                else:
+                    continue
+    if len(boxes) == len(keychain):
+        return True
+    else:
+        return False
